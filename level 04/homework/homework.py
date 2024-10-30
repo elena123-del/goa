923bf268ac0662bb7fb3283853cd39c7664d0076
elena123-del <khatiaozadze@gmail.com>
2024-10-30 18:49:30 +0400

    goa

---
+++ level 04/homework/homework.py
@@ -0,0 +1,42 @@
+points=50
+points=83
+points=47
+
+#snake_case არის წერის სტილი ფითონში და გულისხმობს იმას რომ ჩვენ სიტყვებს გამოვყოფთ ერთმანეთსგან _ ამ დაბლითა ხაზით. მაგალითად ფითონში ვიცით რომ სიტყვებს შორის სფეისს ვერ დავწერთ, ამიტომას ვიყენებს ამ სიმბოლოს.
+
+
+georgia=country
+country="georgia"# swori versia
+
+name=elena
+name="elena"#swori versia
+
+i love Sport
+i_love_sport
+
+i love bike
+i_love_bike
+
+i_hate_programming=True
+i_hate_programming=False # this one is correctt i love programmig
+
+age=10.8
+age=10
+
+height=1,40
+height=1.40
+
+
+number1=777
+number2=10
+print(number1 * number2)
+number3=7770
+
+
+num1=15
+num2=20
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1**1 + num2**2)
