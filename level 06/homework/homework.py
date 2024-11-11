66597aea47324430a7f6d8d57d462a88a36cdbb7
elena123-del <khatiaozadze@gmail.com>
2024-11-11 21:11:01 +0400

    goa

---
+++ level 06/homework/homework.py
@@ -0,0 +1,20 @@
+#მომხმარებელს შემოატანინეთ საყვარელი სპორტი  და ტერმინალში დაბეჭდეთ ის
+fav_sport = input("please enter your favourite sport: ")
+print(fav_sport)
+
+ #მომხმარებელს შეეკითხეთ სახელი, გვარი, ასაკი, საცხოვრებელი ქალაქი,ჰობი შემდეგ კი დაბეჭდეთ გრძელი წინადადება მაგ. შენი სახელია "სახელი და გვარი" შენი ასაკია "ასაკი" და ასე შემდეგ.
+name = input("please enter your name: ")
+surname = input("please enter your surname: ")
+age = int(input("please your age: "))
+city = input("please enter your city: ")
+hobby = input("please enter your hobby: ")
+print(name + " " + surname + " " + str(age) + " " + city + " " + hobby )
+
+#მომხმარებელს შემოატანინეთ ორი რიცხვი  და დაბეჭდეთ მათი ჯამი ,სხვაობა და ნამრავლი . გამოიყენეთ int ფუნქცია.
+number1 = int(input("enter number1: "))
+number2 = int(input("enter number2: "))
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+
+
