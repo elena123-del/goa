53fb9038cb43530036f6e6b7e941713808b4aed1
elena123-del <khatiaozadze@gmail.com>
2024-11-17 20:05:17 +0400

    goa

--- level 06/by ani's homework.py
+++
@@ -1,7 +0,0 @@
-age = int(input("please enter your age: "))
-mothers_age = int(input("please enteryour mothers age: "))
-print(mothers_age - age)
-
-name = input("enter your name: ")
-money = int(input("please enter your money: "))
-print("hi " + name + " you  have " + str(money) + " lari" )
