0fc52f635806ccdf2354b8929edca6defe2dc5b8
elena123-del <khatiaozadze@gmail.com>
2024-11-29 21:37:01 +0400

    goa

--- level 08/homework/homework.py
+++ level 08/homework/homework.py
@@ -1,13 +1,24 @@
 # 1)ჩაწერე განსხვავებული მონაცემთა ტიპების (int, float, string, boolean) ცვლადები.
 
+height = 1.40 #float
+age = 10 #int
+name = "elena" "afanasieva" "goa" #str
+i_love_programmer = True #boolean true
+i_hate_my_family = False #boolean false
+
 # 2)მომხმარებელს შემოატანინეთ მისი ასაკი და type() ფუნქციით შეამოწმეთ,თუ რა ტიპის მონაცემია შემოტანილი ასაკი.
 
 # 3)მომხმარებელს შემოატანინე მისი ასაკი და ბოლოს დაბეჭდე თუ რამდენი წლის იქნება ის 5 წლის შემდეგ
+age = input("please enter ur age: ")
+print(int(age) + 5)
 
 # 4)მომხმარებელს შემოატანინეთ სახელი გვარი ასაკი სიმაღლე საყვარელი ფერი და გამოიტანეთ ეს ყველაფერი ერთ დიდ წინადადებაში.
 name = input("please enter ur name: ")
 surname = input("please enter ur surname: ")
 age = input("please enter ur age: ") 
 height = input("please enter ur height: ")
+fav_color = input("please enter ur fav color: ")
+print("my name is "+ name + " and surname " + surname + " " + " i'm " + age + " years old " + "i'm " + height + " tall and my fav colour is " + fav_color)
 
 # 5)ჩანაწერს აუცილებლად გადახედეთ თავიდან
+#ukve vuyure
