ba2d6f3a5f5cbde72f9b1db678bf917f4ff97ead
elena123-del <khatiaozadze@gmail.com>
2024-11-30 15:06:27 +0400

    goa

--- level 08/homework/homework.py
+++ level 08/homework/homework.py
@@ -1,4 +1,4 @@
-# 1)ჩაწერე განსხვავებული მონაცემთა ტიპების (int, float, string, boolean) ცვლადები.
+ # 1)ჩაწერე განსხვავებული მონაცემთა ტიპების (int, float, string, boolean) ცვლადები.
 
 height = 1.40 #float
 age = 10 #int
@@ -7,8 +7,10 @@ i_love_programmer = True #boolean true
 i_hate_my_family = False #boolean false
 
 # 2)მომხმარებელს შემოატანინეთ მისი ასაკი და type() ფუნქციით შეამოწმეთ,თუ რა ტიპის მონაცემია შემოტანილი ასაკი.
+age = input("please enter ur age: ")
+print(type(age))
 
-# 3)მომხმარებელს შემოატანინე მისი ასაკი და ბოლოს დაბეჭდე თუ რამდენი წლის იქნება ის 5 წლის შემდეგ
+#3)მომხმარებელს შემოატანინე მისი ასაკი და ბოლოს დაბეჭდე თუ რამდენი წლის იქნება ის 5 წლის შემდეგ
 age = input("please enter ur age: ")
 print(int(age) + 5)
 
@@ -21,4 +23,4 @@ fav_color = input("please enter ur fav color: ")
 print("my name is "+ name + " and surname " + surname + " " + " i'm " + age + " years old " + "i'm " + height + " tall and my fav colour is " + fav_color)
 
 # 5)ჩანაწერს აუცილებლად გადახედეთ თავიდან
-#ukve vuyure
+ #ukve vuyure
