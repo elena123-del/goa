ee1ac736dec3d532d465654aa921a289494fab46
elena123-del <khatiaozadze@gmail.com>
2024-12-14 20:38:14 +0400

    goa

--- level 10/homework/homework.py
+++ level 10/homework/homework.py
@@ -1,31 +1,79 @@
 # 1)ივარჯიშეთ შედარების ოპერატორებზე, გააკეთეთ 10-10 მაგალითი. კომენტარებით ახსენით, რა პასუხს გვიბრუნებენ შედარების ოპერატორები.
-print(32<23) =False
-print(41<143) =True
-print(54272<8274) =False
-print(622<2233) =True
-print(3242<22433) =True
-print(3224<223)=False
-print(32342<2423) =False
-print(32790<7023) =False
-print(328<232) =False
-print(325885<26583) =False
-print(5632<2358)=False
-print(5832<2793)=False
-print(32457<4574236) =True
-print(354572<234574745) =True
-print(34682<234574) =True
-print(35682<2653)=False
-print(358682<625683)=True
-print(3258<52385) =True
-print(38562<672853) =True
+print(41<143) #=True
+print(54272<8274) #=False
+print(622<2233) #=True
+print(3242<22433) #=True
+print(3224<223)#=False
+print(32342<2423) #=False
+print(32790<7023) #=False
+print(328<232) #=False
+print(325885<26583) #=False
+print(5632<2358)#=False
+print(5832<2793)#=False
+print(32457<4574236) #=True
+print(354572<234574745) #=True
+print(34682<234574) #=True
+print(35682<2653)#=False
+print(358682<625683)#=True
+print(3258<52385) #=True
+print(38562<672853) #=True
+
+print(41>143) #=False
+print(54272>8274) #=True
+print(622>2233) #=False
+print(3242>22433) #=False
+print(3224>223)#=True
+print(32342>2423) #=True
+print(32790>7023) #=True
+print(328>232) #=True
+print(325885>26583) #=True
+print(5632>2358)#=True
+print(5832>2793)#=True
+
+print(41 >= 143) #=False
+print(54272>=8274) #=True
+print(622>=2233) #=False
+print(3242>=22433) #=False
+print(3224>=223)#=True
+print(32342>=2423) #=True
+print(32790>=7023) #=True
+print(328>=232) #=True
+print(325885>=26583) #=True
+print(5632>=2358)#=True
+print(5832>=2793)#=True
+
+print(41<=143) #=True
+print(54272<=8274) #=False
+print(622<=2233) #=True
+print(3242<=22433) #=True
+print(3224<=223)#=False
+print(32342<=2423) #=False
+print(32790<=7023) #=False
+print(328<=232) #=False
+print(325885<=26583) #=False
+print(5632<=2358)#=False
+print(5832<=2793)#=False
+
+print(41==143) #=False
+print(54272==8274) #=False
+print(622==622) #=True
+print(3242==22433) #=False
+print(3224==3224)#=True
+print(32342==2423) #=False
+print(32790==7023) #=False
+print(232==232) #=True
+print(325885==26583) #=False
+print(5632==2358)#=False
+print(5832==5832)#=True
+
 
 
 # 2)კომენტარებით ახსენით, თუ რა არის explicity და implicity.
 #explicity - პირდაპირად და  implicity არაპირდაპირად
 
 # 3)მომხმარებელს შემოატანინეთ ორი რიცხვი და გაიგეთ მეტია თუ არა პირველი მეოორეზე.
-number = input("please enter ur number: ")
-number1 = input("please enter ur number1: ")
-number < number1 = True
+number = int(input("please enter ur number: "))
+number1 = int(input("please enter ur number1: "))
+print(number > number1)#=True
 
 # 4)ჩანაწერს გადახედეთ თავიდან. okeiiiiiiiii
