90bce41899fd7df811e9ac43a1dc26ad1f8e4ea2
elena123-del <khatiaozadze@gmail.com>
2024-12-23 20:44:28 +0400

    goa

---
+++ level 10/classwork/classwork.py
@@ -0,0 +1,10 @@
+print(234>23) #True
+print(974>4352) #False
+print(345>545) #False
+print(456>45)#True
+print(64544>5646) #True
+print(2345<535) #False
+print(3129>210) #True
+print(327>653)#False
+print(24979>12345) #True
+print(1234567890>987654325454) #False
---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,17 @@
+#შექმენით 10 ცვლადი და მიანიჭთ მნიშვნელობები მაგალითებით (მაგალითად 7 > 9) და შემდეგ დაპრინტეთ True და False სხვა და სხვა კომბინაციით (კომენტარებით მიუწერეთ გვერდით რას გამოიტანს ეკრანზე)
+num1 = 7 >6
+num2 = 109 < 675
+num3 = 637 < 2467
+num4 = 542 < 25
+num5 = 5362 < 2637
+
+print(num1 and num2) #True
+print(num3 and num2) #True
+print(num4 and num1) #false
+print(num2 and num4) #false
+print(num3 and num1) #false
+print(num5 and num1) #false
+print(num5 and num3) #false
+print(num2 and num4) #false
+print(num1 and num4) #false
+print(num3 and num2) # true
