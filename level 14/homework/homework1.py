1b79ce247bed61d1c7f5c6a4374c898f6a8a795b
elena123-del <khatiaozadze@gmail.com>
2025-01-08 23:09:37 +0400

    goa

---
+++ level 14/homework/homework1.py
@@ -0,0 +1,7 @@
+# 3)მომხმარებელს შემოატანინეთ მისი სახელი, შემოატანინეთ მისი ასაკი, ასევე სიმაღლე და შეამოწმეთ თუ მომხმარებლის ასაკი მეტია 18 და მისი სახელი უდრის თქვენს სახელს და ასევე მისი სიმაღლე მეტია 1.80-ზე
+
+name = input("pls enter ur name: ")
+age = int(input("pls enter ur age: "))
+height = float(input("pls enter ur height: "))
+print(age > 18) #false
+print(height > 1.80) #false
