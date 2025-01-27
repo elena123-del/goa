98efe772bd356f3d942537c8b1b1aabde4f21a8e
elena123-del <khatiaozadze@gmail.com>
2025-01-27 14:54:05 +0400

    goa

---
+++ level 16/homework/homework2.py
@@ -0,0 +1,24 @@
+# 6)გააკეთეთ მინიმუმ 5 მაგალითი შედარების ოპერატორებzზე და მინიმუმ 5მაგალითი ლოგიკურ ოპერატორებზე
+
+print(5==8) #false
+print(5!=8) #true
+print(5>8)#false
+print(5<8) #true
+print(5>=8) #false
+print(5<=8) #true
+
+a = 5==5
+b = 6>1
+c = 79>=64
+d = 8==653
+
+print(a or b)#true
+print(b or c)#true
+print(c or d)#true
+
+print(a and b)#true
+print(b and c)#true
+print(c and d)#false
+
+
+
