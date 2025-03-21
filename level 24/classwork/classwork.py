b05555285519b25d07f76c4406653c5db2fc8a96
elena123-del <khatiaozadze@gmail.com>
2025-03-22 00:04:40 +0400

    goa

---
+++ level 24/classwork/classwork.py
@@ -0,0 +1,21 @@
+# 1) მომხმარებელს შემოატანინეთ მთელი რიცხვი და დაწერეთ პროგრამა, რომელიც? ამოწმებს:
+
+# თუ რიცხვი დადებითია, დაბეჭდოს: "the number is positive"
+# თუ უარყოფითია, დაბეჭდოს: "the number is negative"
+# თუ ნულია, დაბეჭდოს: "the number is zero"
+
+# num = int(input("please enter ur num: "))
+
+# if num > 0:
+#     print("the number is positive")
+# elif num < 0:
+#     print("the number is negative")
+# elif num == 0:
+#     print("the num is zero")
+    
+    
+# 2) ცვლადში შეინახეთ პაროლი. შემდეგ მომხმარებელს შემოატანინეთ პაროლი. იქამდე გამოუტანეთ "incorrect password" სანამ მომხმარებელი სწორად არ შემოიტანს პაროლს. სწორად შემოტანის შემთხვევაში გამოიტანეთ "correct password" (გამოიყენეთ while loop)
+
+
+password = "elena123."
+password2 = int(input("enter new password: "))
