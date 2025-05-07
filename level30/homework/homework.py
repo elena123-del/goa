d48bf6a342541f08851917d1b923a335877c8098
elena123-del <khatiaozadze@gmail.com>
2025-05-07 21:44:12 +0400

    goa

---
+++ level30/homework/homework.py
@@ -0,0 +1,22 @@
+# 2) თქვენი სიტყვებით ახსენით განსხვავება Inline, Internal და External CSS შორის. ასევე დაწერეთ თუ რომელს ანიჭებთ უპირატესობას და რატომ.
+
+# inline aris xazshi dawerili css
+# internali aris headshi dawerili css
+# Externali aris calke foldershi dawerili css
+
+# 3) ცვლადში შეინახეთ ნებისმიერი სიტყვა, შემდეგ კი მომხმარებელსაც შემოატანინეთ რაიმე სიტყვა, თუ თქვენი სიტყვები ერთმანეთს დაემთხვევა გამოუტანეთ "Our words are similar !", სხვა შემთხვევაში - "We have different words". hint: დაგჭირდებათ პირობითი განცხადებები და სტრინგის მეთოდები. ეს შემოწმება უნდა იყოს case-insensitive
+
+hello = "hello world"
+
+i = input("enter a word: ")
+
+if hello.lower() == i.lower():
+    print("Our words are similar !")
+
+else:
+    print("We have different words")
+
+# 4) გადაიმეორეთ განვლილი მასალა (Py, HTML, CSS) kk
+
+# 5) შექმენით რეცეპტების Single-page ვებ-საიტი. დაამატეთ ფოტოები, ღილაკები, Heading-ები, Paragraph-ები და ა.შ.
+# გამოიყენეთ აქამდე ნასწავლი Css-ის მასალა, რათა გასტილოთ თქვენი ვებ-საიტი. გამოიყენეთ External CSS. 
